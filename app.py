c0fc53683afec4041ba76a6dd7d95bce1e7d321a
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2023-08-02 19:28:59 +0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+from time import time, sleep
+from thrading import Thread
+
+app=Flask(__name__)
+app.config['SECRET_KEY']='thisIsMySecretKey'
+
+all_ip = {}
+
+@app.route('/')
+def home():
+    ip = request.args.get('ip')
+    if not ip:
+        return "<h1>Insert IP in url parameter as `ip=0.0.0.0`</h1>"
+    if all_ip.get(ip):
+        return 'false'
+    else:
+        all_ip[ip] = time()
+        return 'true'
+    
+def background_task():
+    while True:
+        for ip in list(all_ip):
+            if (time() - all_ip[ip]) > (24 * 60 * 60):
+                del all_ip[ip]
+        sleep(60 * 30)
+
+t=Thread(target=background_task)
+t.daemon=True
+t.start()
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 80, True)
+
