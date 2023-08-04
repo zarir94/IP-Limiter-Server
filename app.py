e7150012b47ea68b14b87ccea71e8b54d2c571f9
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2023-08-04 14:11:36 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,58 @@
 from flask import Flask, request
 from time import time, sleep
 from threading import Thread
+from flask_sqlalchemy import SQLAlchemy
 
-app=Flask(__name__)
-app.config['SECRET_KEY']='thisIsMySecretKey'
+app = Flask(__name__)
 
-all_ip = {}
+# Replace the database URI with your actual PostgreSQL database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tztvvsuw:wUGjVSR5Yutc4eIHGHPqmwBRgA_A9Ify@satao.db.elephantsql.com/tztvvsuw'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the database
+db = SQLAlchemy(app)
+
+class IPAddress(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ip_address = db.Column(db.String(15), unique=True, nullable=False)
+    timestamp = db.Column(db.Float, nullable=False)
+
+# Create the table if it doesn't exist
+db.create_all()
 
 @app.route('/')
 def home():
     ip = request.args.get('ip')
     if not ip:
         return "<h1>Insert IP in url parameter as `ip=0.0.0.0`</h1>"
-    if all_ip.get(ip):
+
+    # Check if the IP exists in the database
+    ip_obj = IPAddress.query.filter_by(ip_address=ip).first()
+
+    if ip_obj:
         return 'false'
     else:
-        all_ip[ip] = time()
+        # Insert the IP into the database
+        timestamp = time()
+        new_ip = IPAddress(ip_address=ip, timestamp=timestamp)
+        db.session.add(new_ip)
+        db.session.commit()
         return 'true'
-    
+
 def background_task():
     while True:
-        for ip in list(all_ip):
-            if (time() - all_ip[ip]) > (24 * 60 * 60):
-                del all_ip[ip]
+        # Remove IP addresses that have been stored for more than 24 hours
+        current_time = time()
+        old_ips = IPAddress.query.filter((current_time - IPAddress.timestamp) > (24 * 60 * 60)).all()
+
+        for ip in old_ips:
+            db.session.delete(ip)
+        db.session.commit()
         sleep(60 * 30)
 
-t=Thread(target=background_task)
-t.daemon=True
+t = Thread(target=background_task)
+t.daemon = True
 t.start()
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 80, True)
-
